ed42e93dba5088280fa62bd5470491cdf990d9c6
activehigh <mahmudul.islam@rokt.com>
2022-07-07 14:20:20 +1000

    Added namesapce in command

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 
 	cfg, options := config.GetManagerParams(scheme,
 		namespace,
-		"istiomerger.monime.sl")
+		"istio.merger.monime.sl")
 	mgr, err := manager.New(cfg, options)
 	if err != nil {
 		log.Fatalf("manager create error: %s", err)
